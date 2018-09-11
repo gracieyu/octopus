4cca1083e38a02227e7d9239292d1e49e18a8d23
Gracie <Gracie@GraciedeMacBook-Pro.local>
2018-09-11 20:35:08 +0800

    first commit

---
+++ Web.py
@@ -0,0 +1,40 @@
+# peoples = [
+#     [2018093,"Emerson"],
+#     [2018094,"Gracie"],
+# ]
+# print("""<head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport"
+#           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+#     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+#     <title>Document</title>
+# </head>""")
+# print("<html>")
+# print("<body>")
+# print("<table><thead><tr><td>学号</td><td>姓名</td><tr></thead>")
+# for row in peoples:
+#     print("<tr>")
+#     print("<td>{}</td><td>{}</td>".format(*row))
+#     print("</tr>")
+#
+# print("</table>")
+# print("</body>")
+# print("</html>")
+
+
+d = {
+    'name': 'Gracie',
+    "lover": "Emerson",
+}
+d.get('o','p')
+
+
+# def copy(dic):
+#     result = {}
+#     keys = dic.keys()
+#     for k in keys:
+#         v = dic[k]
+#         result[k] = v
+#
+#     return result
+
